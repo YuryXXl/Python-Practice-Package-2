32cb6472cf9e32cc790908acd1ab474d624ad8d7
YuryXXl <y7183605555@icloud.com>
2025-03-16 19:32:37 -0600

    exercise three

--- 03_secret_message_decoder/solution.py
+++ 03_secret_message_decoder/solution.py
@@ -1,6 +1,17 @@
+import sys
 # do not change the method name
 def main():
-    pass # remove this line with youir code
+    encoded_message = input("Enter the encoded message: ")
+    
+    step = input("Enter the step size: ")
+
+    if not step.isdigit() or int(step) <= 0:
+        print("Error: Step size must be a positive integer")
+        sys.exit(1)
+    step = int(step)
+    decoded_message = encoded_message[::step]
+    
+    print(f"Decoded Message: {decoded_message}")
 
 # do not change the following lines:    
 if __name__ == "__main__":
